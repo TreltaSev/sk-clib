42a620ffd98f524614e3ffeb31e3ec3ac65d1420
Trelta Sebastion <treltasev@gmail.com>
2025-05-01 11:51:53 -0400

    feat(componenent): Add Flex Component
    Uses Frame to wrap itself

---
+++ src/lib/ui/flex/types.ts
@@ -0,0 +1,5 @@
+import type { tFrameProps } from "../frame/types.js";
+
+export type tFlexProps = tFrameProps & {
+
+};
--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,1 +1,3 @@
+export { default as Flex } from "./flex/components/flex.svelte"
 export { default as Frame } from './frame/components/frame.svelte';
+
