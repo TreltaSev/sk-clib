881c563b0e3faa8ba56787aa0617705935a0eed1
Trelta Sebastion <treltasev@gmail.com>
2025-05-02 17:56:46 -0400

    feat(component): Setup Header Independent Stying
    Added determined semantic element rendering, independent italic, sizing, and weight stylings.

--- src/lib/ui/header/index.ts
+++ src/lib/ui/header/index.ts
@@ -2,4 +2,6 @@ export {default as _, default as Root} from "./components/header.svelte";
 
 export type {
     tHeaderProps as Props,
+    tHeaderSizes as Sizes,
+    tHeaderWeights as Weights
 } from "./types"
