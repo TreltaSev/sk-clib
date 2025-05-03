12ac41cc05a350f54501801ba102e48ef5604cd0
TreltaSev <treltasev@gmail.com>
2025-05-02 22:40:00 -0400

    refactor(library): Importable

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,5 @@
 // Reexport your entry components here
-export * from "./ui"
+export { default as Flex } from "./ui/flex/components/flex.svelte"
+export { default as Frame } from "./ui/frame/components/frame.svelte"
+export { default as Header } from "./ui/header/components/header.svelte"
+export { default as Text } from "./ui/text/components/text.svelte"
