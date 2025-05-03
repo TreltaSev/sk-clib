5ee0f3cf233e6d05a491526dd6a06c0ea381fdb9
TreltaSev <treltasev@gmail.com>
2025-05-02 21:46:39 -0400

    feat(component): Setup Text Component
    Already setup to have very similar logic to the header class

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Flex } from './flex/components/flex.svelte';
 export { default as Frame } from './frame/components/frame.svelte';
 export { default as Header } from './header/components/header.svelte';
+export { default as Text } from "./text/components/text.svelte"
