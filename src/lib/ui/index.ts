6229b37e06ce7acd3978efb0db60a512e9d27204
Trelta Sebastion <treltasev@gmail.com>
2025-05-05 12:15:18 -0400

    feat(configuration): Redid Imports for components

---
+++ src/lib/ui/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
+// Reexport your entry components here
+export { default as Flex } from "./flex/components/flex.svelte"
+export { default as Frame } from "./frame/components/frame.svelte"
+export { default as Header } from "./header/components/header.svelte"
+export { default as Text } from "./text/components/text.svelte"
