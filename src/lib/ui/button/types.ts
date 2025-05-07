4056e8b3a246151959592d87d0553c3f58595fcb
Trelta Sebastion <treltasev@gmail.com>
2025-05-07 11:35:46 -0400

    feat(component): Button, Made class optional

--- src/lib/ui/button/types.ts
+++ src/lib/ui/button/types.ts
@@ -2,5 +2,5 @@ import type { ClassValue, HTMLButtonAttributes } from "svelte/elements";
 
 export type tButtonProps = HTMLButtonAttributes & {
     // Extra Props Here:
-    buttonClass: ClassValue;
+    buttonClass?: ClassValue;
 };
