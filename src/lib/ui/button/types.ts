6db5d9dbb1f71080d170980939fa6494fe86b178
Trelta Sebastion <treltasev@gmail.com>
2025-05-07 10:35:57 -0400

    feat(component): Button Accessibility

--- src/lib/ui/button/types.ts
+++ src/lib/ui/button/types.ts
@@ -1,6 +1,6 @@
-import type { HTMLButtonAttributes } from "svelte/elements";
+import type { ClassValue, HTMLButtonAttributes } from "svelte/elements";
 
 export type tButtonProps = HTMLButtonAttributes & {
     // Extra Props Here:
-    
+    buttonClass: ClassValue;
 };
