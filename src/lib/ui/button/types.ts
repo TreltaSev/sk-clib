38908f32b1ca412e5a5449c4a5f89f6976389219
Trelta Sebastion <treltasev@gmail.com>
2025-05-07 10:20:07 -0400

    feat(component): Setup Button

---
+++ src/lib/ui/button/types.ts
@@ -0,0 +1,6 @@
+import type { HTMLButtonAttributes } from "svelte/elements";
+
+export type tButtonProps = HTMLButtonAttributes & {
+    // Extra Props Here:
+    
+};
--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -2,6 +2,7 @@
 // @ts-nocheck
 
 // Reexport your entry components here
+export { default as Button } from "./button/components/button.svelte"
 export { default as Flex } from "./flex/components/flex.svelte"
 export { default as Frame } from "./frame/components/frame.svelte"
 export { default as Header } from "./header/components/header.svelte"
