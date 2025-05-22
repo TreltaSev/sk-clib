e000dbf11af1fb2b1fe8d510001e74bda066a992
TreltaSev <treltasev@gmail.com>
2025-05-21 20:44:33 -0400

    feat(docs): Add basic doc structure and handler

---
+++ src/lib/docs/_layouts/bare.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Header } from "@lib";
+	let { children, title = $bindable('Not given a title :(') } = $props();
+</script>
+
+<Header xl>{title}</Header>
+
+{@render children?.()}
---
+++ src/lib/docs/contributing/documentation/sub-level/+page.svelte
@@ -0,0 +1,6 @@
+---
+title: This is a sub level
+order: 6
+---
+
+This is a dope sub level
