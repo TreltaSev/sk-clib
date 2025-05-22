f86e223e9b86e264909e8b04249d6bb22cd90b88
TreltaSev <treltasev@gmail.com>
2025-05-21 20:45:32 -0400

    feat(docs): Make doc site less... ugly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 
+	// --- Components ---
+	import { Frame, Flex } from '@lib';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<!-- Navigation Bar -->
+
+<Frame flex col fill class="bg-surface-2 text-white/80">
+	<Flex row fillw centerx class="bg-surface-0 h-12 px-[20%]">
+		<a href="/">sk-clib</a>
+	</Flex>
+
+	<Flex col fill class="px-[20%] mt-20" >
+		{@render children()}
+	</Flex>
+</Frame>
