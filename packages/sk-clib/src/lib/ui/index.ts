ffe2b44523f2d34aa533ad2d64a1c0f3cf987c0a
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-07-08 05:01:42 -0300

    feat(props-export): Export Props

--- packages/sk-clib/src/lib/ui/index.ts
+++ packages/sk-clib/src/lib/ui/index.ts
@@ -2,9 +2,17 @@
 // @ts-nocheck
 
 // Reexport your entry components here
-export { default as Button } from "./button/components/button.svelte"
-export { default as Flex } from "./flex/components/flex.svelte"
-export { default as Frame } from "./frame/components/frame.svelte"
-export { default as Header } from "./header/components/header.svelte"
-export { default as Spacer } from "./spacer/components/spacer.svelte"
-export { default as Text } from "./text/components/text.svelte"
+export { default as Button } from './button/components/button.svelte';
+export { default as Flex } from './flex/components/flex.svelte';
+export { default as Frame } from './frame/components/frame.svelte';
+export { default as Header } from './header/components/header.svelte';
+export { default as Spacer } from './spacer/components/spacer.svelte';
+export { default as Text } from './text/components/text.svelte';
+
+// Reexport component types
+export type { tButtonProps } from './button/types';
+export type { tFlexProps } from './flex/types';
+export type { tFrameProps } from './frame/types';
+export type { tHeaderProps } from './header/types';
+export type { tSpacerProps } from './spacer/types';
+export type { tTextProps } from './text/types';
