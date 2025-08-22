347f2e22e2f46362a4627bbba77bbf95dd488b3c
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2025-08-22 10:00:11 -0400

    chore(playground): Removed playground

--- packages/playground/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import {Frame, Header, Text} from "sk-clib"
-</script>
-
-<Frame class="p-4 box-border">
-    <Header xxl bold>Welcome to sk-clib's playground</Header>
-    <Text>Trust me... this is bare for a reason :)</Text>
-</Frame>
