9d41276ba6b821a32a5dd3f5eba51add74f6ba9c
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2026-01-15 19:30:15 -0500

    feat(button): Optional href button attribute

--- packages/sk-clib/src/lib/ui/button/types.ts
+++ packages/sk-clib/src/lib/ui/button/types.ts
@@ -3,4 +3,6 @@ import type { ClassValue, HTMLButtonAttributes } from "svelte/elements";
 export type tButtonProps = HTMLButtonAttributes & {
     // Extra Props Here:
     buttonClass?: ClassValue;
+
+    href?: string;
 };
