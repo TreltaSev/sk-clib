10e62d295415b0b2036362a37b4207068e26a078
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2026-01-29 10:14:00 -0500

    feat(component): Add input component

--- packages/sk-clib/src/lib/ui/index.ts
+++ packages/sk-clib/src/lib/ui/index.ts
@@ -6,6 +6,7 @@ export { default as Button } from './button/components/button.svelte';
 export { default as Flex } from './flex/components/flex.svelte';
 export { default as Frame } from './frame/components/frame.svelte';
 export { default as Header } from './header/components/header.svelte';
+export { default as Input } from './input/components/input.svelte';
 export { default as Spacer } from './spacer/components/spacer.svelte';
 export { default as Text } from './text/components/text.svelte';
 
@@ -14,5 +15,6 @@ export type { tButtonProps } from './button/types';
 export type { tFlexProps } from './flex/types';
 export type { tFrameProps } from './frame/types';
 export type { tHeaderProps } from './header/types';
+export type { tInputProps } from './input/types'
 export type { tSpacerProps } from './spacer/types';
 export type { tTextProps } from './text/types';
